2749ffff8f6c4ab7bcaa2cfdd8b1d1ef9f3baa9b
Dai Dang Van <daikk115@gmail.com>
2016-05-21 23:14:56 +0700

    change style of special instances

--- cau3.py
+++ cau3.py
@@ -14,22 +14,22 @@ def init_tree():
 	right = Binary(6)
 	a = Binary(5, left, right)
 
-	left = Binary(2)
+	left = Binary(1)
 	right = Binary(4)
 	b = Binary(3, left, right)
 
 	return a, b
 
 def check(a, b):
-	if a is None and b is not None:
-		return False
+	if a is None:
+		if b is None:
+			return True
+		else:
+			return False
+	else:
+		if b is None:
+			False
 
-	if a is not None and b is None:
-		return False
-
-	if a is None and b is None:
-		return True
-	print a.value, b.value
 	if a.value == b.value:
 		if check(a.left, b.left) and check(a.right, b.right):
 			return True
@@ -40,5 +40,4 @@ def check(a, b):
 
 if __name__ == '__main__':
 	a, b = init_tree()
-	print a.value, b.value
 	print(check(a.left, b) or check(a.right, b)	)
